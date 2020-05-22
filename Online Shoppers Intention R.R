106b8f65b25aa9e1f5606e2d2cff08837c750f37
kfaenza <60407713+kfaenza@users.noreply.github.com>
2020-05-21 17:35:43 -0700

    Delete Online Shoppers Intention R.R

--- Online Shoppers Intention R.R
+++
@@ -1,5 +0,0 @@
-url <-"https://raw.githubusercontent.com/kfaenza/Online-Shopper-Intention/master/online_shoppers_intention-2.csv"
-download.file(url,"online_shoppers_intention_2")
-View(online_shoppers_intention_2)
-summary(online_shoppers_intention_2)
-cor(df[1:10])
